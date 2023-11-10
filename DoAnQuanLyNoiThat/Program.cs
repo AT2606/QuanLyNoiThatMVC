c50e02bfdf7dd15fa519597064c5087c83a6900f
AT2606 <0950080156@sv.hcmunre.edu.vn>
2023-11-10 20:27:15 +0700

    first commit

---
+++ DoAnQuanLyNoiThat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DoAnQuanLyNoiThat.View;
+
+namespace DoAnQuanLyNoiThat
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            DatabaseHelper.dbName = "QuanLyNoiThat2";
+            DatabaseHelper.serverName = "MSI\\SQLEXPRESS01";
+            DatabaseHelper.userDb = "sa";
+            DatabaseHelper.password = "2606";
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new login());
+        }
+    }
+}
---
+++ DoAnQuanLyNoiThat/View/login.cs
@@ -0,0 +1,43 @@
+﻿using DoAnQuanLyNoiThat.Controller;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DoAnQuanLyNoiThat.View
+{
+    public partial class login : Form
+    {
+        TaiKhoanController taiKhoanController;
+        public login()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnlogin_Click(object sender, EventArgs e)
+        {
+            string taikhoan = txtTK.Text;
+            string matkhau = txtMk.Text;
+            taiKhoanController = new TaiKhoanController();
+            if (taiKhoanController.KiemtraDN(taikhoan, matkhau))
+            {
+                MDI MDI = new MDI();
+                MDI.Show();
+
+                this.Hide();
+            }
+            else
+            {            
+                    MessageBox.Show("Đăng nhập không thành công. Kiểm tra lại tên đăng nhập và mật khẩu.");
+                    //Application.Exit();
+            }
+
+        }
+    }
+}
